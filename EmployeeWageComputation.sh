f8600d14dd88054cb04ad52cfa27d7750557b8a1
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 16:42:06 +0530

    Calculating Employee Age Per Day.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
+#Calcualating Employee Wage.
 isPresent=$((RANDOM%2))
-if [ $ispresent -eq 1 ]
+wagePerHr=20
+fullDayHrs=8
+
+if [ $isPresent -eq 1 ]
 then
 	echo "Employee is Present."
+	salaryPerDay=$(($wagePerHr*$fullDayHrs))
+	echo "$salaryPerDay"
 else
 	echo "Employee is Absent."
 fi
+
+
+
