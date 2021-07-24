5c8662bc89eaaec6d7dd8ff1f35bb0e1ab3e85fe
mr-a-100125 <gamidiajay@gmail.com>
2021-07-24 16:30:13 +0530

    Checking Employee is Present or Absent.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
+#Check Employee is Present or Absent.
+isPresent=$((RANDOM%2))
+if [ $ispresent -eq 1 ]
+then
+	echo "Employee is Present."
+else
+	echo "Employee is Absent."
+fi
