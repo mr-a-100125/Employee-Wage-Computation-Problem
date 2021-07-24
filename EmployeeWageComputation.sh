5335d2219885d8fe65101ee743ff1bd7a1b4c845
mr-a-100125 <87686518+mr-a-100125@users.noreply.github.com>
2021-07-24 16:43:55 +0530

    Update EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,7 +3,7 @@ echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
 isPresent=$((RANDOM%2))
-if [ $ispresent -eq 1 ]
+if [ $isPresent -eq 1 ]
 then
 	echo "Employee is Present."
 else
