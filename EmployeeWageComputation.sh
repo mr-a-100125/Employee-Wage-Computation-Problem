5e5bd40fa9d34ed43f872a89034b50749b017067
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 16:39:13 +0530

    Calculated monthly salary.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
@@ -8,17 +8,24 @@ wagePerHr=20
 fullDayHrs=8
 partTime=4
 salaryPerDay=0
+workingDayMonth=20
+salaryPerMonth=0
 
 case $isPresent in
 	1) echo "Employee is Full time Present."
 	   salaryPerDay=$(($wagePerHr*$fullDayHrs))
-	   echo "$salaryPerDay"
+	   echo "Day salary : $salaryPerDay"
+	   salaryPerMonth=$(($workingDayMonth*$salaryPerDay))
+	   echo "Month salary : $salaryPerMonth"
 		;;
 	2) echo "Employee is Part time Present."
            salaryPerDay=$(($wagePerHr*$partTime))
-	   echo "$salaryPerDay"
+	   echo "Day salary : $salaryPerDay"
+	   salaryPerMonth=$(($workingDayMonth*$salaryPerDay))
+	   echo "Month salary : $salaryPerMonth"
 		;;
 	*) echo "Employee is Absent."
-	   echo "$salaryPerDay"
+	   echo "Day salary : $salaryPerDay"
+	   echo "Month salary : $salaryPerMonth"
 esac
 
