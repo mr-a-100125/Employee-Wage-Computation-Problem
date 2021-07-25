ff4272610b8bac2c729f67bef1283e14d759455c
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 18:50:14 +0530

    Storing daily wage along with the total wage.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,46 +1,62 @@
-#!/bin/bash 
+#!/bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
 #Calcualating Employee Wage.
-declare -a employee
-pos=0
+
 wagePerHr=20
 fullDayHrs=8
 partTime=4
-absent=0
 salaryPerDay=0
-workingDayMonth=20
+workingDayMonth=40
 salaryPerMonth=0
+absent=0
+present=1
 
-function salary()
+totalHrs=0
+totalDays=0
+declare -a employee
+pos=0
+totalWage=0
+
+function work()
 {
-        salaryPerDay=$(($wagePerHr*$1))
-        totalWorkingHrs=$(($1*$workingDayMonth))
-        if [ $totalWorkingHrs -eq 100 ]
+	salaryPerDay=$(($wagePerHr*$1))
+	totalHrs=$(($totalHrs+$1))
+	totalDays=$(($totalDays+$2))
+	totalWage=$(($totalWage+($salaryPerDay*$totalDays)))
+	employee[pos]="Daily:$salaryPerDay total wage:$(($totalWage))"
+	pos=$((pos+1))
+	if [ $totalHrs -ge 100 ]||[ $totalDays -eq 20 ]
         then
-                salaryPerMonth=$((100*$salaryPerDay))
-        else
-        salaryPerMonth=$(($workingDayMonth*$salaryPerDay))
-        fi
-	employee[pos]="daily:$salaryPerDay totalWage:$salaryPerMonth"
-	pos=$(($pos+1))
-	echo "Daily Wage : $salaryPerDay"
-        echo "total salary : $salaryPerMonth"
+		echo "Salary per Day : $salaryPerDay"
+		salaryPerMonth=$(($salaryPerMonth+$totalDays*$salaryPerDay))
+		echo "Month salary : $salaryPerMonth"
+		return 1
+	fi
 }
 
+for ((i=1; i<=$workingDayMonth;i++))
+do
+	if [ $? -eq 1 ]
+	then
+		break
+	fi
+        isPresent=$((RANDOM%3))
+        case $isPresent in
+                1) echo "Employee is Full time Present."
+                work $fullDayHrs $present
+		        ;;
+                2) echo "Employee is Part time Present."
+                work $partTime $present
+			;;
+                *) echo "Employee is Absent."
+                present=0
+		work $absent $present
+        esac
+done
 
-
-
-isPresent=$((RANDOM%3))
-case $isPresent in
-	1) echo "Employee is Full time Present."
-	   salary $fullDayHrs
-		;;
-	2) echo "Employee is Part time Present."
-           salary $partTime
-		;;
-	*) echo "Employee is Absent."
-	   salary $absent
-esac
-echo ${employee[@]}
+for key in ${!employee[@]}
+do
+	echo ${employee[$key]}
+done
