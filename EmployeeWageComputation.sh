9b985d956d6778ed05641aa4b2059d3923eafae3
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 17:38:32 +0530

    Calculating Monthly wages.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
 #Calcualating Employee Wage.
-isPresent=$((RANDOM%3))
+
 wagePerHr=20
 fullDayHrs=8
 partTime=4
@@ -11,21 +11,24 @@ salaryPerDay=0
 workingDayMonth=20
 salaryPerMonth=0
 
-case $isPresent in
-	1) echo "Employee is Full time Present."
-	   salaryPerDay=$(($wagePerHr*$fullDayHrs))
-	   echo "Day salary : $salaryPerDay"
-	   salaryPerMonth=$(($workingDayMonth*$salaryPerDay))
-	   echo "Month salary : $salaryPerMonth"
-		;;
-	2) echo "Employee is Part time Present."
-           salaryPerDay=$(($wagePerHr*$partTime))
-	   echo "Day salary : $salaryPerDay"
-	   salaryPerMonth=$(($workingDayMonth*$salaryPerDay))
-	   echo "Month salary : $salaryPerMonth"
-		;;
-	*) echo "Employee is Absent."
-	   echo "Day salary : $salaryPerDay"
-	   echo "Month salary : $salaryPerMonth"
-esac
-
+for ((i=1; i<=$workingDayMonth;i++))
+do
+        isPresent=$((RANDOM%3))
+        case $isPresent in
+                1) echo "Employee is Full time Present."
+                salaryPerDay=$(($wagePerHr*$fullDayHrs))
+                echo "Day salary : $salaryPerDay"
+                salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
+                echo "Month salary : $salaryPerMonth"
+                        ;;
+                2) echo "Employee is Part time Present."
+                   salaryPerDay=$(($wagePerHr*$partTime))
+                echo "Day salary : $salaryPerDay"
+                salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
+                echo "Month salary : $salaryPerMonth"
+                        ;;
+                *) echo "Employee is Absent."
+                echo "Day salary : $salaryPerDay"
+                echo "Month salary : $salaryPerMonth"
+        esac
+done
