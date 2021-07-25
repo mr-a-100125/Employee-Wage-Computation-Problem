ebe27e80d70bb0ed40fa99f28723869ce0860e54
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 18:29:48 +0530

    Refactor the Code to write a function to get work hours.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
@@ -7,62 +7,40 @@ echo "Welcome to Employee Wage Computation Program on Master Branch."
 wagePerHr=20
 fullDayHrs=8
 partTime=4
-absent=0
 salaryPerDay=0
 workingDayMonth=20
 salaryPerMonth=0
+absent=0
+present=1
 
 totalHrs=1
 totalDays=0
+
+function work()
+{
+	salaryPerDay=$(($wagePerHr*$1))
+	totalHrs=$(($totalHrs+$1))
+	totalDays=$(($totalDays+$2))
+	if [ $totalHrs -ge 100 ]||[ $totalDays -eq 20 ]
+        then
+		echo "Salary per Day : $salaryPerDay"
+		salaryPerMonth=$(($salaryPerMonth+$salaryPerDay))
+		echo "Month salary : $salaryPerMonth"
+	fi
+}
 
 for ((i=1; i<=$workingDayMonth;i++))
 do
         isPresent=$((RANDOM%3))
         case $isPresent in
                 1) echo "Employee is Full time Present."
-                salaryPerDay=$(($wagePerHr*$fullDayHrs))
-                echo "Day salary : $salaryPerDay"
-                salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
-                echo "Month salary : $salaryPerMonth"
-                        ;;
+                work $fullDayHrs $present
+		        ;;
                 2) echo "Employee is Part time Present."
-                   salaryPerDay=$(($wagePerHr*$partTime))
-                echo "Day salary : $salaryPerDay"
-                salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
-                echo "Month salary : $salaryPerMonth"
-		totalHrs=$(($totalHrs+$fullDayHrs))
-		totalDays=$(($totalDays+1))
-		if [ $totalHrs -ge 100 ]||[ $totalDays -eq 20 ]
-                then
-			echo "Salary per Day : $salaryPerDay"
-			salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
-			echo "Month salary : $salaryPerMonth"
-			break;
-		fi
-                        ;;
-                2) echo "Employee is Part time Present."
-                salaryPerDay=$(($wagePerHr*$partTime))
-                totalHrs=$(($totalHrs+$partTime))
-                totalDays=$(($totalDays+1))
-                if [ $totalHrs -ge 100 ]||[ $totalDays -eq 20 ]
-                then
-                        echo "Salary per Day : $salaryPerDay"
-                        salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
-                        echo "Month salary : $salaryPerMonth"
-                        break;
-                fi
-                        ;;
+                work $partTime $present
+			;;
                 *) echo "Employee is Absent."
-                echo "Day salary : $salaryPerDay"
-                echo "Month salary : $salaryPerMonth"
-		totalHrs=$(($totalHrs+0))
-                totalDays=$(($totalDays+0))
-                if [ $totalHrs -ge 100 ]||[ $totalDays -eq 20 ]
-                then
-                        echo "Salary per Day : $salaryPerDay"
-                        salaryPerMonth=$(($salaryPerMonth+$workingDayMonth*$salaryPerDay))
-                        echo "Month salary : $salaryPerMonth"
-                        break;
-                fi
+                present=0
+		work $absent $present
         esac
 done
