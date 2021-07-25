c56a6e21c8bef096311a3fab238f0711796ecdcf
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 18:27:51 +0530

    Refactor the Code to write a function to get work hours.

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,36 +3,44 @@ echo "Welcome to Employee Wage Computation Program on Master Branch."
 
 #Check Employee is Present or Absent.
 #Calcualating Employee Wage.
-isPresent=$((RANDOM%3))
+
 wagePerHr=20
 fullDayHrs=8
 partTime=4
-absent=0
 salaryPerDay=0
 workingDayMonth=20
 salaryPerMonth=0
+absent=0
+present=1
 
-function salary()
+totalHrs=1
+totalDays=0
+
+function work()
 {
-        salaryPerDay=$(($wagePerHr*$1))
-        echo "Day salary : $salaryPerDay"
-        totalWorkingHrs=$(($1*$workingDayMonth))
-        if [ $totalWorkingHrs -eq 100 ]
+	salaryPerDay=$(($wagePerHr*$1))
+	totalHrs=$(($totalHrs+$1))
+	totalDays=$(($totalDays+$2))
+	if [ $totalHrs -ge 100 ]||[ $totalDays -eq 20 ]
         then
-                salaryPerMonth=$((100*$salaryPerDay))
-        else
-        salaryPerMonth=$(($workingDayMonth*$salaryPerDay))
-        fi
-        echo "Month salary : $salaryPerMonth"
+		echo "Salary per Day : $salaryPerDay"
+		salaryPerMonth=$(($salaryPerMonth+$salaryPerDay))
+		echo "Month salary : $salaryPerMonth"
+	fi
 }
 
-case $isPresent in
-	1) echo "Employee is Full time Present."
-	   salary $fullDayHrs
-		;;
-	2) echo "Employee is Part time Present."
-           salary $partTime
-		;;
-	*) echo "Employee is Absent."
-	   salary $absent
-esac
+for ((i=1; i<=$workingDayMonth;i++))
+do
+        isPresent=$((RANDOM%3))
+        case $isPresent in
+                1) echo "Employee is Full time Present."
+                work $fullDayHrs $present
+		        ;;
+                2) echo "Employee is Part time Present."
+                work $partTime $present
+			;;
+                *) echo "Employee is Absent."
+                present=0
+		work $absent $present
+        esac
+done
